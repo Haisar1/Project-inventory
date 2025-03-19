50a0d7e20ad3e7daa94dbbf111c8f5560c85ff7b
Haisar <anthonysarmiento044@gmail.com>
2025-03-18 21:04:54 -0500

    create front

---
+++ inventory-front/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'machines', loadChildren: () => import('./machines/machines.module').then(m => m.MachinesModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
